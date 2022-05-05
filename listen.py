bc73b102a546b7995da703cc9af833b0542c2e91
home laptop <vu.dzyubin@gmail.com>
2022-05-05 22:40:35 +0300

    separate file for Monitor class

--- listen.py
+++ listen.py
@@ -3,6 +3,7 @@
 # запускающий ТД поток
 
 from Logger import Logger
+from Monitor import Monitor
 import subprocess
 from time import sleep
 from threading import Thread, Lock, Timer
@@ -15,57 +16,6 @@ lock = Lock()
 cache = []
 
 dump_num = 5
-
-
-class Monitor():
-    '''Newtork entity of airmon-ng for promiscuous listening'''
-    def __init__(self, logger):
-        '''
-        input:
-            logger - logging object
-        fields:
-            logger
-
-        '''
-        self.log_client = logger
-
-    @property
-    def logger(self):
-        return self.log_client
-
-    def get_mon_iface_name(self, iwfaces):
-        from re import findall
-        try:
-            new_iface = findall(r'monitor\smode\s.+enabled\son\s.+', iwfaces)[0]
-            new_iface = findall(r'[a-z0-9]+$', new_iface)[0]
-            return new_iface
-
-        except Exception as e:
-            lock.acquire()
-            self.log_client.addToLine('Monitors.get_mon_iface_name',\
-                "no new monitor interface found " + str(e) + '" with ' + iwfaces, 'error')
-            lock.release()
-            return ''
-
-    def push(self, netw_iface):
-        '''
-        add new monitor
-        input:
-            netw_iface - name of used network interface
-        output:
-            new monitor interface 
-        '''
-        import subprocess
-        from random import randint
-        air_out = subprocess.run(["airmon-ng", "start", netw_iface, str(randint(1, 14))], capture_output=True)
-        air_out = air_out.stdout.decode('utf-8')
-        new_iface = self.get_mon_iface_name(air_out)
-        return new_iface
-
-    def pop(self, mon):
-        import subprocess
-        subprocess.run(["airmon-ng", "stop", mon], capture_output=False)
-
 
 
 def start_AP(essid, bssid, netw_iface, path, channel):
